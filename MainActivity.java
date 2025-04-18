d12cf203b0606f0f9856d4bd51e5ea5d27ea004a
Mahmoud Khalid <142645144+mahmoud3x0@users.noreply.github.com>
2025-04-18 19:47:43 +0200

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.app1;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn_open = findViewById(R.id.btn_id);
+        btn_open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_DIAL);
+                EditText call = findViewById(R.id.call);
+                String s = call.getText().toString();
+                Uri uri = Uri.parse("tel:" + s);
+                intent.setData(uri);
+                startActivity(intent);
+            }
+        });
+    }
+}
