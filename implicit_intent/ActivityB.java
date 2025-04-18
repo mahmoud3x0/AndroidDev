5f6ea3f88f787015d2c839152c9531628ca0a87c
Mahmoud Khalid <142645144+mahmoud3x0@users.noreply.github.com>
2025-04-18 18:29:09 +0200

    Add files via upload

---
+++ implicit_intent/ActivityB.java
@@ -0,0 +1,34 @@
+package com.example.app1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ActivityB extends AppCompatActivity {
+
+    TextView text_view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_b);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent intent = getIntent();
+        String s = intent.getStringExtra("name");
+        text_view = findViewById(R.id.text_view_id);
+        text_view.setText(s);
+
+    }
+}
